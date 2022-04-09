4844d5465fded0627ebbf32fc53b7eba387d88bd
albuquerquealdry <albuquerquealdry@gmail.com>
2022-04-09 16:35:43 -0300

    first commit

---
+++ maping-deployment.py
@@ -0,0 +1,10 @@
+import re
+import json
+import os
+IMAGE = "IMAGEAPP"
+APLICATION = "APPLICATIONAPP"
+PORT = "PORTAPP"
+os.system(f"sed -i 's,*app-name,{APLICATION},g' deployment.yaml")
+os.system(f"sed -i 's,*app-images,{IMAGE},g' deployment.yaml")
+os.system(f"sed -i 's,*app-port,{PORT},g' deployment.yaml")
+os.system("kubectl --kubeconfig=k8s-1-21-5-do-0-sfo3-1643064513184-kubeconfig.yaml apply -f deployment.yaml")
